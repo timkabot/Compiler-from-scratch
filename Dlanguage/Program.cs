4524fc005e3ed41b78d0c5694b0b4e7cf9f53143
Timur Borgalinov <borgalinov@gmail.com>
2018-11-11 19:13:31 +0300

    first step done

---
+++ Dlanguage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dlanguage
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tokenizer tokenizer = new Tokenizer();
+            List<DslToken> tokens = tokenizer.Tokenize("var t := []; t[10] := 25; t[100] := func(x)=>x+1;  t[1000] := {a:=1,b:=2.7};");
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                Console.WriteLine( "(TokenType :" +  tokens[i].TokenType + ", TokenValue: " + tokens[i].Value +")");
+
+            }
+        }
+    }
+}
